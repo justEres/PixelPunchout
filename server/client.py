d16c82ab96539acb1005dd91bd6610bfc98c0f04
Dragon07112007 <120335816+Dragon07112007@users.noreply.github.com>
2023-11-04 21:30:09 +0100

    Server Basics.
    Nicht Implementiert

---
+++ server/client.py
@@ -0,0 +1,25 @@
+import socket
+from time import sleep
+from threading import Thread
+
+def recv(server:socket.socket):
+    while True:
+        buffer = server.recv(1024)
+        buffer = buffer.decode("utf-8")
+        print(buffer)
+if __name__ == "__main__":
+    ip = "127.0.0.1"
+    port = 1234
+
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.connect((ip, port))
+
+    thread = Thread(target=recv, args=(server,))
+    thread.start()
+
+    string = "haskklfj"
+    sleep(20)
+    server.send(bytes(string, "utf-8"))
+
+
+
---
+++ server/server.py
@@ -0,0 +1,50 @@
+import queue
+import socket
+from threading import Thread
+
+from queue import Queue
+
+
+def clientSendData(client:socket.socket, qDict, address):
+    q = qDict[address]
+
+    while True:
+        data = q.get()
+        client.send(bytes(data, "utf-8"))
+
+def clientHandler(client:socket.socket, address, qDict):
+    print(f"Connection Established - {address}")
+    q = qDict[address]
+    thread = Thread(target=clientSendData, args=(client, qDict, address))
+    thread.start()
+    while True:
+        string = client.recv(1024)
+        if not string: break
+        string = string.decode("utf-8")
+        for i in qDict:
+            if i == address:
+                continue
+            else:
+               qDict[i].put(string)
+    print("Client Disconnected")
+
+if __name__ == "__main__":
+    ip = "127.0.0.1"
+    port = 1234
+    connection_list = []
+    q = Queue()
+    qDict = {}
+
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((ip, port))
+    server.listen(5)
+
+    while True:
+        client, address = server.accept()
+        address = f"{address[0]}:{address[1]}"
+        qDict[address] = Queue()
+
+        thread = Thread(target=clientHandler, args = (client, address, qDict))
+        thread.start()
+
+
